0ace5a7f91e5750b591a5d5239514e16e450121a
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-09-14 00:15:57 +0800

    product id

--- components/Pay/payModule.js
+++ components/Pay/payModule.js
@@ -12,7 +12,6 @@ import { useState } from "react";
 import { usePayContext } from "context/payContext";
 import { useNotificationContext } from "context/notificationContext";
 import { useRouter } from "next/navigation";
-import { Modal } from "antd";
 
 const PayModule = (props) => {
   const { visible, setVisible, checkPayment, isPaid, invoice } =
@@ -66,154 +65,171 @@ const PayModule = (props) => {
 
   return (
     <>
-      <Modal
-        open={visible}
-        title="Дансаар эсвэл Qpay ашиглах"
-        width={1000}
-        footer={[]}
-        onCancel={() => setVisible(false)}
+      <div
+        className={`modal-root  ${
+          visible === false ? "displayNone" : "displayOn"
+        }`}
       >
-        <div tabindex="0" aria-hidden="true"></div>
-        <div className="payModal-content">
-          <div className="payModal-body">
-            <div className="modal-body">
-              {loading === true && (
-                <div className="loader-box">
-                  <div class="loader">Loading...</div>
-                </div>
-              )}
-              <div className="modal-header">
-                <div className="left-section">
-                  <div className="modal-title">
-                    <h3> Дансаар эсвэл Qpay ашиглах </h3>
-                    <div className="modal-sub-title">
-                      Аль ч банкны аппликейшн ашиглан уншуулж болно.
+        <div className="pay-modal-mask" onClick={() => setVisible(false)}></div>
+        <div className="pay-modal-wrap pay-modal-centered">
+          <div className="payModal">
+            <div tabindex="0" aria-hidden="true"></div>
+            <div className="payModal-content">
+              <div className="payModal-body">
+                <button
+                  className="close"
+                  type="button"
+                  onClick={() => setVisible(false)}
+                >
+                  <FontAwesomeIcon icon={faXmark} />
+                </button>
+                <div className="modal-body">
+                  {loading === true && (
+                    <div className="loader-box">
+                      <div class="loader">Loading...</div>
                     </div>
-                  </div>
-                </div>
-              </div>
-              <div className="modal-content">
-                <div className="qpay">
-                  <h5>QR код уншуулах</h5>
-                  {invoice && (
-                    <img src={`data:image/png;base64,${invoice.qr_image}`} />
                   )}
-                  <button className="qpay-btn" onClick={() => paymentCheck()}>
-                    Төлбөр шалгах{" "}
-                  </button>
-                </div>
-                <div className="banks">
-                  <h5>Дансаар шилжүүлэх</h5>
-                  <div className="banks-header">
-                    {banks &&
-                      banks.map((bank, index) => (
-                        <div
-                          className={`tab-choise ${
-                            acitveTab === index && "active"
-                          }`}
-                          onClick={() => setActiveTab(index)}
-                        >
-                          {bank.bankName}
+                  <div className="modal-header">
+                    <div className="left-section">
+                      <div className="modal-title">
+                        <h3> Дансаар эсвэл Qpay ашиглах </h3>
+                        <div className="modal-sub-title">
+                          Аль ч банкны аппликейшн ашиглан уншуулж болно.
                         </div>
-                      ))}
+                      </div>
+                    </div>
                   </div>
-                  <div className="bank-invoice">
-                    <div className="item">
-                      <div className="sub"> Хүлээн авах данс</div>
-                      <div className="typography">
-                        <span>
-                          {banks &&
-                            banks[acitveTab] &&
-                            banks[acitveTab].bankAccount}
-                        </span>
-                        <button
-                          onClick={() => {
-                            toastControl("success", "Хуулагдлаа");
-                            navigator.clipboard.writeText(
-                              banks &&
+                  <div className="modal-content">
+                    <div className="qpay">
+                      <h5>QR код уншуулах</h5>
+                      {invoice && (
+                        <img
+                          src={`data:image/png;base64,${invoice.qr_image}`}
+                        />
+                      )}
+                      <button
+                        className="qpay-btn"
+                        onClick={() => paymentCheck()}
+                      >
+                        Төлбөр шалгах{" "}
+                      </button>
+                    </div>
+                    <div className="banks">
+                      <h5>Дансаар шилжүүлэх</h5>
+                      <div className="banks-header">
+                        {banks &&
+                          banks.map((bank, index) => (
+                            <div
+                              className={`tab-choise ${
+                                acitveTab === index && "active"
+                              }`}
+                              onClick={() => setActiveTab(index)}
+                            >
+                              {bank.bankName}
+                            </div>
+                          ))}
+                      </div>
+                      <div className="bank-invoice">
+                        <div className="item">
+                          <div className="sub"> Хүлээн авах данс</div>
+                          <div className="typography">
+                            <span>
+                              {banks &&
                                 banks[acitveTab] &&
-                                banks[acitveTab].bankAccount
-                            );
-                          }}
-                        >
-                          <FontAwesomeIcon icon={faCopy} />
-                        </button>
-                      </div>
-                    </div>
-                    <div className="item">
-                      <div className="sub"> Хүлээн авагч</div>
-                      <div className="typography">
-                        <span>
-                          {banks &&
-                            banks[acitveTab] &&
-                            banks[acitveTab].accountName}{" "}
-                        </span>
-                        <button
-                          onClick={() => {
-                            toastControl("success", "Хуулагдлаа");
-                            navigator.clipboard.writeText(
-                              banks &&
+                                banks[acitveTab].bankAccount}
+                            </span>
+                            <button
+                              onClick={() => {
+                                toastControl("success", "Хуулагдлаа");
+                                navigator.clipboard.writeText(
+                                  banks &&
+                                    banks[acitveTab] &&
+                                    banks[acitveTab].bankAccount
+                                );
+                              }}
+                            >
+                              <FontAwesomeIcon icon={faCopy} />
+                            </button>
+                          </div>
+                        </div>
+                        <div className="item">
+                          <div className="sub"> Хүлээн авагч</div>
+                          <div className="typography">
+                            <span>
+                              {banks &&
                                 banks[acitveTab] &&
-                                banks[acitveTab].accountName
-                            );
-                          }}
-                        >
-                          <FontAwesomeIcon icon={faCopy} />
-                        </button>
+                                banks[acitveTab].accountName}{" "}
+                            </span>
+                            <button
+                              onClick={() => {
+                                toastControl("success", "Хуулагдлаа");
+                                navigator.clipboard.writeText(
+                                  banks &&
+                                    banks[acitveTab] &&
+                                    banks[acitveTab].accountName
+                                );
+                              }}
+                            >
+                              <FontAwesomeIcon icon={faCopy} />
+                            </button>
+                          </div>
+                        </div>
+                        <div className="item">
+                          <div className="sub"> Төлөх дүн</div>
+                          <div className="typography">
+                            <span>
+                              {props.invoice &&
+                                new Intl.NumberFormat().format(
+                                  props.invoice.total
+                                )}
+                              ₮
+                            </span>
+                            <button
+                              onClick={() => {
+                                toastControl("success", "Хуулагдлаа");
+                                navigator.clipboard.writeText(
+                                  props.invoice && props.invoice.total
+                                );
+                              }}
+                            >
+                              <FontAwesomeIcon icon={faCopy} />
+                            </button>
+                          </div>
+                        </div>
+                        <div className="item">
+                          <div className="sub"> Гүйлгээний утга</div>
+                          <div className="typography">
+                            <span>
+                              {props.invoice && props.invoice.orderNumber}
+                            </span>
+                            <button
+                              onClick={() => {
+                                toastControl("success", "Хуулагдлаа");
+                                navigator.clipboard.writeText(
+                                  props.invoice && props.invoice.orderNumber
+                                );
+                              }}
+                            >
+                              <FontAwesomeIcon icon={faCopy} />
+                            </button>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <div className="item">
-                      <div className="sub"> Төлөх дүн</div>
-                      <div className="typography">
-                        <span>
-                          {props.invoice &&
-                            new Intl.NumberFormat().format(props.invoice.total)}
-                          ₮
-                        </span>
-                        <button
-                          onClick={() => {
-                            toastControl("success", "Хуулагдлаа");
-                            navigator.clipboard.writeText(
-                              props.invoice && props.invoice.total
-                            );
-                          }}
-                        >
-                          <FontAwesomeIcon icon={faCopy} />
-                        </button>
-                      </div>
-                    </div>
-                    <div className="item">
-                      <div className="sub"> Гүйлгээний утга</div>
-                      <div className="typography">
-                        <span>
-                          {props.invoice && props.invoice.orderNumber}
-                        </span>
-                        <button
-                          onClick={() => {
-                            toastControl("success", "Хуулагдлаа");
-                            navigator.clipboard.writeText(
-                              props.invoice && props.invoice.orderNumber
-                            );
-                          }}
-                        >
-                          <FontAwesomeIcon icon={faCopy} />
-                        </button>
-                      </div>
-                    </div>
+                  </div>
+                  <div className="alert alert-warning qpay-info">
+                    Банкны дансаар шилжүүлсэн бол ажлын өдрүүдэд шалган
+                    баталгаажуулах болно <br />
+                    <br /> <strong> Qpay - ээр </strong>Төлбөрөө төлсөн бол
+                    "ТӨЛБӨР ШАЛГАХ" товч дээр дарна уу. Жич: Төлсөн ч төлөөгүй
+                    гэсэн хариу өгч байвал дахин шалгаарай.
                   </div>
                 </div>
-              </div>
-              <div className="alert alert-warning qpay-info">
-                Банкны дансаар шилжүүлсэн бол ажлын өдрүүдэд шалган
-                баталгаажуулах болно <br />
-                <br /> <strong> Qpay - ээр </strong>Төлбөрөө төлсөн бол "ТӨЛБӨР
-                ШАЛГАХ" товч дээр дарна уу. Жич: Төлсөн ч төлөөгүй гэсэн хариу
-                өгч байвал дахин шалгаарай.
               </div>
             </div>
           </div>
         </div>
-      </Modal>
+      </div>
     </>
   );
 };
